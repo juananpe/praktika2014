ed4eecc595a8c57b7633e304aa96570e0dc09cf9
juanan <juanan@juanan-MacBookPro>
2014-09-30 23:03:50 +0200

    Hello ikutu egin dut kautotu adarrean

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -2,7 +2,7 @@
 public class Hello {
 
 	/**
-	 * Hello World!
+	 * Hello World! (kautotu-n zerbait ikutu dugu)
 	 * @param args metodo nagusian erabiliko diren parametroak. 
 	 */
 	public static void main(String[] args) {
