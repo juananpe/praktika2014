dcbe3f8fa21d637fd7481995e0d23e71678fb6ab
Juanan Pereira <juanan.pereira@ehu.es>
2014-09-30 16:04:33 +0200

    Bigarren bertsioa / main metodoan komentarioak sartzen ditu

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -1,6 +1,10 @@
 
 public class Hello {
 
+	/**
+	 * Hello World!
+	 * @param args metodo nagusian erabiliko diren parametroak. 
+	 */
 	public static void main(String[] args) {
 		System.out.println("Kaixo");
 	}
