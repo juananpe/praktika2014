618d2e87895e2fcf6371b6757faa9655200e2a86
juanan <juanan@juanan-MacBookPro>
2014-09-30 23:05:26 +0200

    kautotun lanean jarraitzen dugu

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -2,7 +2,7 @@
 public class Hello {
 
 	/**
-	 * Hello World! (kautotu-n zerbait ikutu dugu)
+	 * Hello World! (kautotu-n zerbait ikutu dugu eta merge egin baino lehen, master-en ere ikutu egingo dugu)
 	 * @param args metodo nagusian erabiliko diren parametroak. 
 	 */
 	public static void main(String[] args) {
